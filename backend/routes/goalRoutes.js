c84da555792d6d98f8e6c85241c1a897546379ae
Sensei <criswiz52@gmail.com>
2022-04-08 08:23:31 +0000

    errorHandler , asyncHandler and async

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,25 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const {getGoals, setGoals, updateGoal, deleteGoal} = require('../controllers/goalController')
 
-/*Get goals endpoint */
-router.get('/', (req, res) => {
-    res.status(200).json({message: 'Get Goal'})
-})
 
-router.post('/', (req, res) => {
-    // if(!req.body.text){
-    //     res.status('404')
-    // }
-    res.status(200).json({message: 'set Goal'})
-})
+router.route('/').get(getGoals).post(setGoals)
 
-router.put('/:id', (req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`})
-})
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
-})
+router.route('/:id').put(updateGoal).delete(deleteGoal)
 
 
 module.exports = router
